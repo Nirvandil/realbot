e858b857afebe6a784ab630fd12d3b38df564bba
Nirvandil <nirvandil@gmail.com>
2018-03-20 10:30:25 +0300

    Added JDBC to get balance by phone number. Removed Web dependencies.

--- src/main/java/cf/nirvandil/realbot/commands/BalanceCommand.java
+++ src/main/java/cf/nirvandil/realbot/commands/BalanceCommand.java
@@ -1,37 +1,36 @@
-/* Commented out for future development.
-
 package cf.nirvandil.realbot.commands;
 
 import cf.nirvandil.realbot.callbacks.LoggingCallback;
 import cf.nirvandil.realbot.model.UserData;
 import cf.nirvandil.realbot.repo.UserDataRepo;
-import cf.nirvandil.realbot.service.RealApiClient;
 import cf.nirvandil.realbot.service.SendMessageFactory;
 import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Lazy;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Component;
 import org.telegram.telegrambots.api.objects.Chat;
 import org.telegram.telegrambots.api.objects.User;
 import org.telegram.telegrambots.bots.AbsSender;
-import org.telegram.telegrambots.exceptions.TelegramApiException;
+
+import java.util.List;
 
 @Slf4j
 @Component
 public class BalanceCommand extends BaseBotCommand {
     private final SendMessageFactory messageFactory;
-    private final RealApiClient realApiClient;
     private final UserDataRepo userDataRepo;
+    private final JdbcTemplate jdbcTemplate;
 
     @Autowired
     public BalanceCommand(@Lazy SendMessageFactory messageFactory,
-                          RealApiClient realApiClient,
-                          UserDataRepo userDataRepo) {
+                          UserDataRepo userDataRepo,
+                          JdbcTemplate jdbcTemplate) {
         super("balance", "Получить баланс лицевого счёта.");
         this.messageFactory = messageFactory;
-        this.realApiClient = realApiClient;
         this.userDataRepo = userDataRepo;
+        this.jdbcTemplate = jdbcTemplate;
         this.isPublic = false;
     }
 
@@ -39,38 +38,16 @@ public class BalanceCommand extends BaseBotCommand {
     @SneakyThrows
     protected void handleCommand(AbsSender absSender, User user, Chat chat, String[] arguments) {
         Long chatId = chat.getId();
-        UserData userData = userDataRepo.findById(user.getId()).orElseThrow(RuntimeException::new);
+        UserData userData = userDataRepo.findById(user.getId())
+                .orElseThrow(RuntimeException::new);
         String phone = userData.getPhone();
-        realApiClient.findClientIdByPhone(phone)
-                .doOnNext(findIdResponse -> {
-                    log.debug("Response withId: {}", findIdResponse);
-                    if (findIdResponse.isSuccess()) {
-                        realApiClient.findCustomerDataById(findIdResponse.getId())
-                                .doOnNext(dataResponse -> {
-                                    log.debug("Response with customer data: {}", dataResponse);
-                                    try {
-                                        if (dataResponse.isSuccess()) {
-                                            Double balance = dataResponse.getBalance();
-                                            absSender.executeAsync(messageFactory.successBalanceMessage(chatId, balance),
-                                                    new LoggingCallback<>());
-                                        } else {
-                                            log.warn("Can't get customer data.");
-                                            absSender.executeAsync(messageFactory.failAccessDataMessage(chatId),
-                                                    new LoggingCallback<>());
-                                        }
-                                    } catch (TelegramApiException e) {
-                                        log.error("Error sending get balance message, exc: {}", e);
-                                    }
-                                })
-                                .subscribe();
-                    } else try {
-                        absSender.executeAsync(messageFactory.failAccessDataMessage(chatId), new LoggingCallback<>());
-                    } catch (TelegramApiException e) {
-                        log.error("Error sending get balance message, exc: {}", e);
-                    }
-                })
-                .subscribe();
+        List<Double> balance = jdbcTemplate.query(
+                "SELECT id, first_name, last_name FROM customers WHERE first_name = ?", new Object[]{phone},
+                (rs, rowNum) -> rs.getDouble("balance")
+        );
+        if (!balance.isEmpty()) {
+            absSender.executeAsync(messageFactory.successBalanceMessage(chatId, balance.get(0)),
+                    new LoggingCallback<>());
+        }
     }
 }
-
-*/
--- src/main/java/cf/nirvandil/realbot/config/ApplicationConfig.java
+++ src/main/java/cf/nirvandil/realbot/config/ApplicationConfig.java
@@ -1,32 +1,15 @@
 package cf.nirvandil.realbot.config;
 
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.reactive.function.client.ClientRequest;
-import org.springframework.web.reactive.function.client.ClientResponse;
-import org.springframework.web.reactive.function.client.ExchangeFunction;
-import org.springframework.web.reactive.function.client.WebClient;
-import org.springframework.web.util.DefaultUriBuilderFactory;
-import org.springframework.web.util.UriBuilder;
 import org.telegram.telegrambots.bots.DefaultBotOptions;
 import org.telegram.telegrambots.bots.commandbot.commands.BotCommand;
 import org.telegram.telegrambots.bots.commandbot.commands.helpCommand.HelpCommand;
-import reactor.core.publisher.Mono;
-
-import static org.springframework.http.HttpHeaders.ACCEPT;
-import static org.springframework.http.MediaType.APPLICATION_JSON_UTF8_VALUE;
 
 @Slf4j
 @Configuration
 public class ApplicationConfig {
-    @Value("${bot.realApiHost}")
-    private String realApiHost;
-    @Value("${bot.realApiPath}")
-    private String realApiPath;
-    @Value("${bot.realApiKey}")
-    private String realApiKey;
 
     @Bean
     public DefaultBotOptions botOptions() {
@@ -41,35 +24,4 @@ public class ApplicationConfig {
                 "Показывает информацию об остальных командах.");
     }
 
-    @Bean
-    public WebClient webClient() {
-        WebClient.Builder builder = WebClient.builder()
-                .uriBuilderFactory(authorizedRealFactory())
-                .defaultHeader(ACCEPT, APPLICATION_JSON_UTF8_VALUE);
-        if (log.isDebugEnabled()) builder.filter(this::loggingFilter);
-        return builder.build();
-    }
-
-    private DefaultUriBuilderFactory authorizedRealFactory() {
-        return new DefaultUriBuilderFactory() {
-            @Override
-            public UriBuilder builder() {
-                return super.builder().scheme("https").host(realApiHost).path(realApiPath)
-                        .queryParam("key", realApiKey);
-            }
-        };
-    }
-
-    private Mono<ClientResponse> loggingFilter(ClientRequest request, ExchangeFunction next) {
-        log.debug("===============Dumping client request===============");
-        log.trace("Request: {}", request);
-        log.debug("Request URI: {}", request.url());
-        log.debug("Request method: {}", request.method());
-        log.debug("Request attributes: {}", request.attributes());
-        log.debug("Request headers: {}", request.headers());
-        log.debug("Request cookies: {}", request.cookies());
-        log.debug("Request body: {}", request.body());
-        log.debug("==============End of client request dump=============");
-        return next.exchange(request);
-    }
 }
